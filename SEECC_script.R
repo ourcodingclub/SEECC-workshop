e129dc18e7891c5bd03b9a52828c82700578e9cb
johngodlee <johngodlee@gmail.com>
2017-03-16 13:16:52 +0000

    maps of class distributions

--- SEECC_script.R
+++ SEECC_script.R
@@ -10,6 +10,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # Load data ----
 load("LPIUKall.RData")
 load("LPIdata_Feb2016.RData")
+  # load("GBIF.RData")
 
 # Clean data ----
 ## Remove whitespace in variable names 
@@ -30,10 +31,19 @@ LPI_UK_class_summ <- LPIdata_Feb2016_UK %>%
 ### Create dot plot
 ggplot(LPI_UK_class_summ, aes(x = Class, y = n)) + 
   geom_point(aes(colour = Class), size = 5)
+
+### Maps of class spatial distributions
+ggplot(LPIdata_Feb2016, aes(x = Decimal_Longitude, y = Decimal_Latitude, colour = Class)) + geom_point()
+ggplot(LPIdata_Feb2016_UK, aes(x = Decimal_Longitude, y = Decimal_Latitude, colour = Class)) + geom_point()
+
+### How does the length of time the population has been studied affect the slope estimate?
+ggplot(LPIUKall, aes(x = lengthyear, y = slope)) + 
+  geom_point()
+
 # Create ggmaps of record distributions ----
 ## Single species
 
 ## Automate for all species using *apply()
 
-
+?geom_line
 
